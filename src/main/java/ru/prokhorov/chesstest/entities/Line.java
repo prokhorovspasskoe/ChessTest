2e3c3f806417b9812fe4d9077dff1eeb8c1c64a0
sergey <prokhorovspasskoe@yandex.ru>
2022-05-14 23:35:46 +0300

    Change line

--- src/main/java/ru/prokhorov/chesstest/entities/Line.java
+++ src/main/java/ru/prokhorov/chesstest/entities/Line.java
@@ -2,10 +2,28 @@ package ru.prokhorov.chesstest.entities;
 
 import ru.prokhorov.chesstest.interfaces.ChessPiece;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
-    public void setObjectToLine(ChessPiece chessPiece, int len) {
+    private final List<ChessPiece> objectsOnLine;
+    private int len;
+    public Line() {
+        this.objectsOnLine = new ArrayList<>();
     }
 
-    public void setLen(int len) {
+    public void setObjectToLine(ChessPiece chessPiece, int len) {
+        this.len = len;
+        objectsOnLine.add(chessPiece);
+    }
+    public ChessPiece getPieceInCellOnLine(int index){
+        return objectsOnLine.get(index);
+    }
+    public List<ChessPiece> getObjectsOnLine() {
+        return objectsOnLine;
+    }
+
+    public int getLen() {
+        return len;
     }
 }
